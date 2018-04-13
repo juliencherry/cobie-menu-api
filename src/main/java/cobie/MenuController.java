0b4c9ec586f56ab10ac8f6fe4034e4c8c13fb2e8
Julien Cherry <juliencherry@gmail.com>
2018-04-13 10:55:07 -0400

    Return status 500 when can’t get menu

--- src/main/java/cobie/MenuController.java
+++ src/main/java/cobie/MenuController.java
@@ -3,19 +3,21 @@ package cobie;
 import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RestController
 public class MenuController {
 
 	@RequestMapping(value = "/menu/today", method = RequestMethod.GET, produces = "application/json")
-	public String menu() {
+	public String menu(HttpServletResponse response) {
 		MenuScraper scraper = new MenuScraper();
 		Gson gson = new Gson();
 
 		try {
 			return gson.toJson(scraper.getTodaysMenu());
 		} catch (IOException e) {
+			response.setStatus(500);
 			return "{\"response\": \"500\"}";
 		}
 	}
